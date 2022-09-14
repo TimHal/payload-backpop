6feb98270d2fecdff86b32fd9d449935a41521bd
thallybu <tim.hallyburton@motionminers.de>
2022-09-14 18:15:41 +0200

    fix: formatting

--- demo/src/hooks/backpopulate-cleanup.hook.ts
+++ demo/src/hooks/backpopulate-cleanup.hook.ts
@@ -3,120 +3,120 @@ import { AfterDeleteHook } from "payload/dist/collections/config/types";
 import { FieldHook } from "payload/types";
 
 export interface BackpopulateCleanupHookArgs {
-    source_field: string;
-    target_slug: string;
-    target_field: string;
+  source_field: string;
+  target_slug: string;
+  target_field: string;
 }
-
 
 //When the parent field is deleted
 export const backpopulateCleanupHookFactory = ({
-                                            source_field,
-                                            target_field,
-                                            target_slug,
-                                        }: BackpopulateCleanupHookArgs): AfterDeleteHook => {
-    const cleanupHook = async ({ req, id, doc }) => {
-        // query all documents which have a relationship to this document
-        console.log("backpopulateCleanupHookFactory hook")
-        let value = doc[source_field] ? doc[source_field] : [];
-        if (value && value.length >= 1 && value[0].value) {
-            let newValue = [];
-            for (const valueEntry of value) {
-                newValue.push(valueEntry.value);
-            }
-            value = newValue;
-        }
-        console.log("value", value)
+  source_field,
+  target_field,
+  target_slug,
+}: BackpopulateCleanupHookArgs): AfterDeleteHook => {
+  const cleanupHook = async ({ req, id, doc }) => {
+    // query all documents which have a relationship to this document
+    console.log("backpopulateCleanupHookFactory hook");
+    let value = doc[source_field] ? doc[source_field] : [];
+    if (value && value.length >= 1 && value[0].value) {
+      let newValue = [];
+      for (const valueEntry of value) {
+        newValue.push(valueEntry.value);
+      }
+      value = newValue;
+    }
+    console.log("value", value);
 
-        for (let targetId of value) {
-            const targetDocument = await payload.findByID({
-                collection: target_slug,
-                id: targetId,
-            });
-            if(!targetDocument){
-                continue;
-            }
-            console.log("Found target document: ", targetDocument)
+    for (let targetId of value) {
+      const targetDocument = await payload.findByID({
+        collection: target_slug,
+        id: targetId,
+      });
+      if (!targetDocument) {
+        continue;
+      }
+      console.log("Found target document: ", targetDocument);
 
-            // get the current backrefs
-            const prevReferences = targetDocument[target_field].map((ref) => ref.id);
+      // get the current backrefs
+      const prevReferences = targetDocument[target_field].map((ref) => ref.id);
 
-            // remove self from backrefs
-            await payload.update({
-                collection: target_slug,
-                id: targetId,
-                overrideAccess: true,
-                data: {
-                    [target_field]: prevReferences.filter((id) => id && id !== doc.id),
-                },
-            });
-        }
-    };
+      // remove self from backrefs
+      await payload.update({
+        collection: target_slug,
+        id: targetId,
+        overrideAccess: true,
+        data: {
+          [target_field]: prevReferences.filter((id) => id && id !== doc.id),
+        },
+      });
+    }
+  };
 
-    return cleanupHook;
+  return cleanupHook;
 };
-
 
 //When the backpopulated field is deleted
 export const parentCleanupHookFactory = ({
-                                            source_field,
-                                            target_field,
-                                            target_slug,
-                                        }: BackpopulateCleanupHookArgs): AfterDeleteHook => {
-    const cleanupHook = async ({ req, id, doc }) => {
-        // query all documents which have a relationship to this document
-        console.log("backpopulateCleanupHookFactory hook")
-        let value = doc[source_field] ? doc[source_field] : [];
-        if (value && value.length >= 1 && value[0].value) {
-            let newValue = [];
-            for (const valueEntry of value) {
-                newValue.push(valueEntry.value);
-            }
-            value = newValue;
-        }
-        console.log("value", value)
-        console.log("id", id)
+  source_field,
+  target_field,
+  target_slug,
+}: BackpopulateCleanupHookArgs): AfterDeleteHook => {
+  const cleanupHook = async ({ req, id, doc }) => {
+    // query all documents which have a relationship to this document
+    console.log("backpopulateCleanupHookFactory hook");
+    let value = doc[source_field] ? doc[source_field] : [];
+    if (value && value.length >= 1 && value[0].value) {
+      let newValue = [];
+      for (const valueEntry of value) {
+        newValue.push(valueEntry.value);
+      }
+      value = newValue;
+    }
+    console.log("value", value);
+    console.log("id", id);
 
+    for (let targetId of value) {
+      const targetDocument = await payload.findByID({
+        collection: target_slug,
+        id: targetId,
+      });
+      if (!targetDocument) {
+        continue;
+      }
+      console.log(
+        "Found targetDocument[target_field]: ",
+        targetDocument[target_field]
+      );
 
-        for (let targetId of value) {
-            const targetDocument = await payload.findByID({
-                collection: target_slug,
-                id: targetId,
-            });
-            if(!targetDocument){
-                continue;
-            }
-            console.log("Found targetDocument[target_field]: ", targetDocument[target_field])
+      // get the current backrefs
+      const prevReferences = targetDocument[target_field].map((ref) =>
+        ref.id ? ref.id : ref.value.id ? ref.value.id : ref.value
+      );
 
-            // get the current backrefs
-            const prevReferences = targetDocument[target_field].map((ref) => ref.id ? ref.id : (ref.value.id ? ref.value.id : ref.value));
+      console.log("prevReferences", prevReferences);
 
-            console.log("prevReferences", prevReferences)
+      let updatedReferenceIds = [];
+      updatedReferenceIds = prevReferences.filter((ref) => {
+        console.log("REF", ref.id ? ref.id : ref);
+        console.log("id", id);
 
-            let updatedReferenceIds = [];
-            updatedReferenceIds = prevReferences.filter(
-                (ref) => {
-                    console.log("REF", (ref.id ? ref.id : ref) );
-                    console.log("id", id );
+        return (ref.id ? ref.id : ref) !== id; //Sometimes doc is the id, sometimes doc.id is the id
+      });
 
-                    return (ref.id ? ref.id : ref) !== id //Sometimes doc is the id, sometimes doc.id is the id
-                }
-            );
+      console.log("updatedReferenceIds", updatedReferenceIds);
 
-            console.log("updatedReferenceIds", updatedReferenceIds)
+      // remove self from backrefs
+      console.log("Target field", target_field);
+      await payload.update({
+        collection: target_slug,
+        id: targetId,
+        overrideAccess: true,
+        data: {
+          [target_field]: updatedReferenceIds, //TODO: Doesnt work. Not properly removed from parent relaitonship yet (esp if parents field is array-like relaitonship, non-poly of course)
+        },
+      });
+    }
+  };
 
-            // remove self from backrefs
-            console.log("Target field", target_field)
-            await payload.update({
-                collection: target_slug,
-                id: targetId,
-                overrideAccess: true,
-                data: {
-                    [target_field]: updatedReferenceIds, //TODO: Doesnt work. Not properly removed from parent relaitonship yet (esp if parents field is array-like relaitonship, non-poly of course)
-                },
-            });
-        }
-    };
-
-    return cleanupHook;
+  return cleanupHook;
 };
--- demo/src/hooks/backpopulate.hook.ts
+++ demo/src/hooks/backpopulate.hook.ts
@@ -1,144 +1,119 @@
 import payload from "payload";
-import {Field, FieldHook} from "payload/types";
-import {hookArgs} from "./backpopulate";
+import { FieldHook } from "payload/types";
+import { hookArgs } from "./backpopulate";
 
-export const backpopulateAfterChangeHookFactory = ({ //If value is added or updated from relationship?
-                                                       targetCollection,
-                                                       backpopulatedField,
-                                                       originalField,
-                                                   }: hookArgs) => {
-    const hook: FieldHook = async (args) => {
-        console.log("Running simple hook...")
+export const backpopulateAfterChangeHookFactory = ({
+  //If value is added or updated from relationship?
+  targetCollection,
+  backpopulatedField,
+  originalField,
+}: hookArgs) => {
+  const hook: FieldHook = async (args) => {
+    const { operation, originalDoc, value, previousValue } = args;
 
-        let {operation, originalDoc, value, previousValue} = args;
-        console.log("After change value", value)
-        console.log("After change previousValue", previousValue)
+    if (operation === "create" || operation === "update") {
+      if (value === undefined || value === null) {
+        // This should never happen, but better safe than sorry.
+        return;
+      }
 
+      console.log("Running simple hook");
+      //If the relationTo "value" is an array with length 1: Usually: Value [ '6307772a5aa9f04ab75df7d4' ] with this: [ { relationTo: 'gear-component', value: '6307772a5aa9f04ab75df7d4' } ]
 
-        if (operation === "create" || operation === "update") {
-            console.log("Create or update operation...: ", operation)
+      const removedTargetIds = previousValue
+        ? [...previousValue].filter((x) => !value.includes(x))
+        : [];
+      const addedTargetIds = value.filter(
+        (x) => !(previousValue ?? []).includes(x)
+      );
 
-            //If the relationTo "value" is an array with length 1: Usually: Value [ '6307772a5aa9f04ab75df7d4' ] with this: [ { relationTo: 'gear-component', value: '6307772a5aa9f04ab75df7d4' } ]
-            if (value && value.length >= 1 && value[0].value) {
-                let newValue = [];
-                for (const valueEntry of value) {
-                    newValue.push(valueEntry.value);
-                }
-                value = newValue;
-            }
-            if (previousValue && previousValue.length >= 1 && previousValue[0].value) {
-                let newValue = [];
-                for (const valueEntry of previousValue) {
-                    newValue.push(valueEntry.value);
-                }
-                previousValue = newValue;
-            }
+      console.log("added", addedTargetIds);
+      console.log("removed", removedTargetIds);
 
-            let removedTargetIds: string[] = [];
-            let addedTargetIds: string[] = [];
-            //Get removed IDs
-            if(previousValue){
-                for(const previousValueEntry of previousValue){
-                    if(!value.includes(previousValueEntry)){
-                        removedTargetIds.push(previousValueEntry);
-                    }
-                }
-            }
-
-            //Get added IDs
-            if(value){
-                for(const newValueEntry of value){
-                    if(!previousValue.includes(newValueEntry)){
-                        addedTargetIds.push(newValueEntry);
-                    }
-                }
-            }
-
-
-            console.log("Added target IDs: ", addedTargetIds)
-            console.log("Removed target IDs: ", removedTargetIds)
-
-            const documentsToRemoveBackPop = removedTargetIds.length==0 ? [] : (await payload.find({
+      const documentsToRemoveBackPop =
+        removedTargetIds.length == 0
+          ? []
+          : (
+              await payload.find({
                 collection: targetCollection.slug,
                 overrideAccess: true,
                 depth: 1,
                 pagination: false,
                 limit: 100000,
                 where: {
-                    id: {
-                        in: removedTargetIds,
-                    }
-                }
-            })).docs;
-            console.log("documentsToRemoveBackPop: ", documentsToRemoveBackPop)
+                  id: {
+                    in: removedTargetIds,
+                  },
+                },
+              })
+            ).docs;
 
-            const documentsToAddBackPop = addedTargetIds.length==0 ? [] : (await payload.find({
+      const documentsToAddBackPop =
+        addedTargetIds.length == 0
+          ? []
+          : (
+              await payload.find({
                 collection: targetCollection.slug,
                 overrideAccess: true,
                 depth: 1,
                 pagination: false,
                 limit: 100000,
                 where: {
-                    id: {
-                        in: addedTargetIds,
-                    }
-                }
-            })).docs;
-            console.log("documentsToAddBackPop: ", documentsToAddBackPop)
+                  id: {
+                    in: addedTargetIds,
+                  },
+                },
+              })
+            ).docs;
 
+      for (const documentToRemoveBackPop of documentsToRemoveBackPop) {
+        // this document is not referenced (any more) make sure the originalDoc is not included in the target field
+        console.log(
+          "Found targetdocument whose backpoulation should be removed",
+          documentToRemoveBackPop.id
+        );
 
-            for (let documentToRemoveBackPop of documentsToRemoveBackPop) {
-                let updatedReferenceIds = [];
+        const prevReferencedIds = documentToRemoveBackPop[
+          backpopulatedField["name"]
+        ].map((doc) => doc.id);
 
-                // this document is not referenced (any more) make sure the originalDoc is not included in the target field
-                console.log("Found targetdocument whose backpoulation should be removed", documentToRemoveBackPop.id);
+        const updatedReferenceIds = prevReferencedIds.filter((doc) => {
+          return (doc.id ? doc.id : doc) !== originalDoc.id; //Sometimes doc is the id, sometimes doc.id is the id
+        });
 
-                const prevReferencedIds = documentToRemoveBackPop[backpopulatedField["name"]].map(doc => doc.id);
+        await payload.update({
+          collection: targetCollection.slug,
+          id: documentToRemoveBackPop.id,
+          overrideAccess: true,
+          data: {
+            [backpopulatedField["name"]]: updatedReferenceIds,
+          },
+        });
+      }
 
-                updatedReferenceIds = prevReferencedIds.filter(
-                    (doc) => {
-                        return (doc.id ? doc.id : doc) !== originalDoc.id //Sometimes doc is the id, sometimes doc.id is the id
-                    }
-                );
+      for (const documentToAddBackPop of documentsToAddBackPop) {
+        const prevReferencedIds = documentToAddBackPop[
+          backpopulatedField["name"]
+        ].map((doc) => doc.id);
+        const updatedReferenceIds = Array.from(
+          new Set([...prevReferencedIds, originalDoc.id])
+        );
+        await payload.update({
+          collection: targetCollection.slug,
+          id: documentToAddBackPop.id,
+          overrideAccess: true,
+          data: {
+            [backpopulatedField["name"]]: updatedReferenceIds,
+          },
+        });
+      }
+    }
 
-                await payload.update({
-                    collection: targetCollection.slug,
-                    id: documentToRemoveBackPop.id,
-                    overrideAccess: true,
-                    data: {
-                        [backpopulatedField["name"]]: updatedReferenceIds,
-                    },
-                });
-            }
+    return; //NOT return value; as the new value of that field doesn't change because of this hook anyways!!! Returning value works usually,
+    // but not when the relationTo field is a simple backpopulate thingy but an ARRAY with the length of 1. Due to the previous value
+    // conversion there, we cannot just return value again as the format is for non-array relationTO's now and not for array relationTo#s.
+    // Thus, better to save the pain and just use a simple return;
+  };
 
-
-            for (let documentToAddBackPop of documentsToAddBackPop) {
-                let updatedReferenceIds = [];
-
-                console.log("Found targetdocument which should be backpopulated", documentToAddBackPop.id);
-                // this is one of the referenced documents, we want to append ourselves to the field, but only once
-
-                const prevReferencedIds = documentToAddBackPop[backpopulatedField["name"]].map(doc => doc.id);
-                updatedReferenceIds = Array.from(
-                    new Set([...prevReferencedIds, originalDoc.id])
-                );
-                await payload.update({
-                    collection: targetCollection.slug,
-                    id: documentToAddBackPop.id,
-                    overrideAccess: true,
-                    data: {
-                        [backpopulatedField["name"]]: updatedReferenceIds,
-                    },
-                });
-            }
-
-        }
-
-        return; //NOT return value; as the new value of that field doesn't change because of this hook anyways!!! Returning value works usually,
-        // but not when the relationTo field is a simple backpopulate thingy but an ARRAY with the length of 1. Due to the previous value
-        // conversion there, we cannot just return value again as the format is for non-array relationTO's now and not for array relationTo#s.
-        // Thus, better to save the pain and just use a simple return;
-    };
-
-    return hook;
+  return hook;
 };
