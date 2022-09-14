8847f3140770f88c311c812aaaca4a2c73265075
thallybu <tim.hallyburton@motionminers.de>
2022-09-14 18:15:30 +0200

    fix: formatting, imports

--- demo/src/payload.config.ts
+++ demo/src/payload.config.ts
@@ -1,30 +1,22 @@
-import { buildConfig } from 'payload/config';
-import path from 'path';
-// import Examples from './collections/Examples';
-import Users from './collections/Users';
-import Foo from './collections/Foo.collection';
-import Bar from './collections/Bar.collection';
-import BackpopulatedRelationshipsPlugin from './backpopulated-relationships.plugin';
-import Baz from './collections/Baz.collection';
+import { buildConfig } from "payload/config";
+import path from "path";
+import Users from "./collections/Users";
+import Foo from "./collections/Foo.collection";
+import Bar from "./collections/Bar.collection";
+import Baz from "./collections/Baz.collection";
+import BackpopulatedRelationshipsPlugin from "./backpopulated-relationship.plugin";
 
 export default buildConfig({
-  serverURL: 'http://localhost:3000',
+  serverURL: "http://localhost:3000",
   admin: {
     user: Users.slug,
   },
-  collections: [
-    Users,
-    Foo,
-    Bar,
-    Baz,
-  ],
+  collections: [Users, Foo, Bar, Baz],
   typescript: {
-    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+    outputFile: path.resolve(__dirname, "payload-types.ts"),
   },
   graphQL: {
-    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
+    schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
   },
-  plugins: [
-    BackpopulatedRelationshipsPlugin
-  ]
+  plugins: [BackpopulatedRelationshipsPlugin],
 });
