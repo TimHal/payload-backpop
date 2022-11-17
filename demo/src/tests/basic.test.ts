ebcbfca893c46f37583ffb5cf3ddd9b709f5f26b
TimHal <tim.hallyburton@gmx.de>
2022-11-17 06:29:24 +0100

    feat: add poly remove test

--- demo/src/tests/basic.test.ts
+++ demo/src/tests/basic.test.ts
@@ -4,7 +4,7 @@ import Bar from "../collections/Bar.collection";
 import Baz from "../collections/Baz.collection";
 import Foo from "../collections/Foo.collection";
 
-describe("basic tests", () => {
+describe("Basic Tests", () => {
   beforeAll(async () => {
     const mongo = await MongoMemoryServer.create();
 
@@ -114,6 +114,52 @@ describe("basic tests", () => {
     ).toBe(0);
   });
 
+  test("Delete Simple Relationships", async () => {
+    // make sure the backpopulated fields are updated correctly when
+    // the source element gets deleted
+
+    const foo = await payload.create({
+      collection: Foo.slug,
+      data: { name: "Foo 1" },
+    });
+    const bar = await payload.create({
+      collection: Bar.slug,
+      data: { name: "Bar 1" },
+    });
+
+    await payload.update({
+      collection: Foo.slug,
+      id: foo.id,
+      data: {
+        bars: [bar.id],
+      },
+    });
+
+    expect(
+      (
+        await payload.findByID({
+          collection: Bar.slug,
+          id: bar.id,
+        })
+      ).foo_bars_backpopulated.length
+    ).toBe(1);
+
+    // now delete the foo element and expect the backpopulated field to be empty again
+    await payload.delete({
+      collection: Foo.slug,
+      id: foo.id,
+    });
+
+    expect(
+      (
+        await payload.findByID({
+          collection: Bar.slug,
+          id: bar.id,
+        })
+      ).foo_bars_backpopulated.length
+    ).toBe(0);
+  });
+
   test("Polymorphic Relationships", async () => {
     // set up instances of foos and bazzes
     const foo_1 = await payload.create({
@@ -123,6 +169,16 @@ describe("basic tests", () => {
     const foo_2 = await payload.create({
       collection: Foo.slug,
       data: { name: "Foo 2" },
+    });
+
+    const bar_1 = await payload.create({
+      collection: Bar.slug,
+      data: { name: "Bar 1" },
+    });
+
+    const ba2_1 = await payload.create({
+      collection: Bar.slug,
+      data: { name: "Ba2 1" },
     });
 
     const baz_1 = await payload.create({
@@ -187,6 +243,44 @@ describe("basic tests", () => {
         await payload.findByID({
           collection: Baz.slug,
           id: baz_2.id,
+        })
+      ).foo_bars_or_bazzes_backpopulated.length
+    ).toBe(1);
+
+    // finally, try 1 baz and 1 bar together
+    //
+    await payload.update({
+      collection: Foo.slug,
+      id: foo_1.id,
+      data: {
+        bars_or_bazzes: [
+          { value: baz_1.id, relationTo: Baz.slug },
+          { value: bar_1.id, relationTo: Bar.slug },
+        ],
+      },
+    });
+
+    expect(
+      (
+        await payload.findByID({
+          collection: Foo.slug,
+          id: foo_1.id,
+        })
+      ).bars_or_bazzes.length
+    ).toBe(2);
+    expect(
+      (
+        await payload.findByID({
+          collection: Baz.slug,
+          id: baz_1.id,
+        })
+      ).foo_bars_or_bazzes_backpopulated.length
+    ).toBe(1);
+    expect(
+      (
+        await payload.findByID({
+          collection: Bar.slug,
+          id: bar_1.id,
         })
       ).foo_bars_or_bazzes_backpopulated.length
     ).toBe(1);
