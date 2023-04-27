992160de91ddecf2d57f5e78296879f33639e02a
TimHal <tim.hallyburton@gmx.de>
2023-04-27 22:20:31 +0200

    feat: backpop simple deletion, wording

--- src/tests/simple/simple.spec.ts
+++ src/tests/simple/simple.spec.ts
@@ -25,7 +25,7 @@ describe("Simple Config Tests", () => {
     handle.close();
   });
 
-  it("Should backpopulate a simple relationship [single add, single delete]", async () => {
+  it("Should backpopulate a simple relationship [single add, single remove]", async () => {
     // Create basic entities
     let foo = await payload.create({
       collection: fooSlug,
@@ -68,6 +68,132 @@ describe("Simple Config Tests", () => {
       data: {
         bars: [],
       },
+    });
+
+    bar = await payload.findByID({
+      collection: barSlug,
+      id: bar.id,
+    });
+
+    expect(bar).toMatchObject({
+      name: bar.name,
+      foo_bars_backpopulated: [],
+    });
+  });
+
+  it("Should handle multiple active relationships [many add, many remove]", async () => {
+    // Create foo1, foo2, foo3 and bar1, bar2
+    let bar1 = await payload.create({
+      collection: barSlug,
+      data: { name: "bar1" },
+    });
+    let bar2 = await payload.create({
+      collection: barSlug,
+      data: { name: "bar2" },
+    });
+
+    let foo1 = await payload.create({
+      collection: fooSlug,
+      data: {
+        name: "foo1",
+      },
+    });
+    let foo2 = await payload.create({
+      collection: fooSlug,
+      data: {
+        name: "foo2",
+        bars: [bar1.id, bar2.id],
+      },
+    });
+    let foo3 = await payload.create({
+      collection: fooSlug,
+      data: {
+        name: "foo3",
+        bars: [],
+      },
+    });
+
+    // Assert that backpopulation are what we expect
+    bar1 = await payload.findByID({ collection: barSlug, id: bar1.id });
+    bar2 = await payload.findByID({ collection: barSlug, id: bar2.id });
+
+    expect(bar1.foo_bars_backpopulated).toMatchObject([foo2]);
+    expect(bar2.foo_bars_backpopulated).toMatchObject([foo2]);
+
+    await payload.update({
+      collection: fooSlug,
+      id: foo2.id,
+      data: { bars: [] },
+    });
+
+    // Assert that backpopulation are what we expect
+    bar1 = await payload.findByID({ collection: barSlug, id: bar1.id });
+    bar2 = await payload.findByID({ collection: barSlug, id: bar2.id });
+
+    expect(bar1.foo_bars_backpopulated).toMatchObject([]);
+    expect(bar2.foo_bars_backpopulated).toMatchObject([]);
+
+    await payload.update({
+      collection: fooSlug,
+      id: foo3.id,
+      data: { bars: [bar1.id, bar2.id] },
+    });
+    await payload.update({
+      collection: fooSlug,
+      id: foo1.id,
+      data: { bars: [bar1.id] },
+    });
+
+    // Assert that backpopulation are what we expect
+    bar1 = await payload.findByID({ collection: barSlug, id: bar1.id });
+    bar2 = await payload.findByID({ collection: barSlug, id: bar2.id });
+    foo1 = await payload.findByID({ collection: fooSlug, id: foo1.id });
+    foo3 = await payload.findByID({ collection: fooSlug, id: foo3.id });
+
+    expect(bar1.foo_bars_backpopulated).toMatchObject([foo3, foo1]);
+    expect(bar2.foo_bars_backpopulated).toMatchObject([foo3]);
+  });
+
+  it("Should handle deletion of target elements [single add, single delete]", async () => {
+    // Create basic entities
+    let foo = await payload.create({
+      collection: fooSlug,
+      data: {
+        name: "foo",
+        bars: [],
+      },
+    });
+
+    let bar = await payload.create({
+      collection: barSlug,
+      data: {
+        name: "bar",
+      },
+    });
+
+    // Now connect foo and bar, bar should backpopulate the relationship
+    foo = await payload.update({
+      collection: fooSlug,
+      id: foo.id,
+      data: {
+        bars: [bar.id],
+      },
+    });
+
+    bar = await payload.findByID({
+      collection: barSlug,
+      id: bar.id,
+    });
+
+    expect(bar).toMatchObject({
+      name: bar.name,
+      foo_bars_backpopulated: [foo],
+    });
+
+    // Delete the foo and check again
+    await payload.delete({
+      collection: fooSlug,
+      id: foo.id,
     });
 
     bar = await payload.findByID({
